bc192f3d67279d3c1f2dbad364105820a879e299
Hao Tran <git@haothitran.com>
2018-08-03 16:51:43 -0400

    Change global string to symbols

--- CombatStatus.lua
+++ CombatStatus.lua
@@ -32,9 +32,9 @@ f:RegisterEvent('PLAYER_REGEN_DISABLED')
 f:SetScript('OnEvent', function(self, event, subev, arg2, arg3)
   -- leaving combat
 	if event == 'PLAYER_REGEN_ENABLED' then
-		s:AddMessage('-- '..COMBAT, 0, 1, 0)
+		s:AddMessage('-----', 0, 1, 0)
   -- entering combat
 	elseif event == 'PLAYER_REGEN_DISABLED' then
-		s:AddMessage('++ '..COMBAT, 1, 0, 0)
+		s:AddMessage('+++++', 1, 0, 0)
 	end
 end)
