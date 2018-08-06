5807d7019924267954f659f94acb36562381587f
Hao Tran <git@haothitran.com>
2018-08-06 02:22:08 -0400

    Convert single quotes to double quotes to be consistent with Blizzard

--- CombatStatus.lua
+++ CombatStatus.lua
@@ -2,11 +2,11 @@
 -- // COMBAT STATUS
 --------------------------------------------------------------------------------
 
-local position = {'CENTER', UIParent, 'CENTER', 0, -120}
+local position = {"CENTER", UIParent, "CENTER", 0, -120}
 local font = STANDARD_TEXT_FONT   -- font family
 local fontSize = 12               -- font size
-local fontStyle = 'THINOUTLINE'   -- font style
-local textAlign = 'CENTER'        -- text justification
+local fontStyle = "THINOUTLINE"   -- font style
+local textAlign = "CENTER"        -- text justification
 local visibletime = 2             -- how long indicator is visible
 local fadeTime = .3								-- how long it takes for indicator to fade
 
@@ -14,7 +14,7 @@ local fadeTime = .3								-- how long it takes for indicator to fade
 -- // STATUS FRAME
 --------------------------------------------------------------------------------
 
-local s = CreateFrame('ScrollingMessageFrame', 'KlazCombatStatus', UIParent)
+local s = CreateFrame("ScrollingMessageFrame", "KlazCombatStatus", UIParent)
 s:SetPoint(unpack(position))
 s:SetWidth(200)
 s:SetHeight(fontSize)
@@ -26,15 +26,15 @@ s:SetMaxLines(1)
 s:SetTimeVisible(visibletime)
 s:SetFadeDuration(fadeTime)
 
-local f = CreateFrame('Frame')
-f:RegisterEvent('PLAYER_REGEN_ENABLED')
-f:RegisterEvent('PLAYER_REGEN_DISABLED')
-f:SetScript('OnEvent', function(self, event, subev, arg2, arg3)
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_REGEN_ENABLED")
+f:RegisterEvent("PLAYER_REGEN_DISABLED")
+f:SetScript("OnEvent", function(self, event, subev, arg2, arg3)
   -- leaving combat
-	if event == 'PLAYER_REGEN_ENABLED' then
-		s:AddMessage('-----', 0, 1, 0)
+	if event == "PLAYER_REGEN_ENABLED" then
+		s:AddMessage("-----", 0, 1, 0)
   -- entering combat
-	elseif event == 'PLAYER_REGEN_DISABLED' then
-		s:AddMessage('+++++', 1, 0, 0)
+	elseif event == "PLAYER_REGEN_DISABLED" then
+		s:AddMessage("+++++", 1, 0, 0)
 	end
 end)
